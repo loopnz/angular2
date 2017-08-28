d635ef2b7413ef19795e484f8cd88770291daa85
cp <286420375@qq.com>
2017-08-28 17:42:43 +0800

    hello world complete

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+	Component, 
+	OnInit,
+	Input
+	} from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,10 +11,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-	name:string;
+	@Input() name:string;
 
   constructor() {
-  		this.name="tom";
+  		
    }
 
   ngOnInit() {
